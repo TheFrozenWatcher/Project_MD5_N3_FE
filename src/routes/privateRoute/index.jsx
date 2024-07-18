b99297a5ee682927dee1e6d34317a9c63c634d9e
TrongKD <khuatdinhtrong@gmail.com>
2024-07-18 09:19:33 +0700

    Task 8944 done

--- src/routes/privateRoute/index.jsx
+++ src/routes/privateRoute/index.jsx
@@ -1,10 +1,11 @@
 import PrivateRoute from "../../features/protectedRouter/PrivateRoute";
 import LayoutIndex from "../../layouts";
+import AdminIndex from "../../pages/admin";
 
 const privateRoutes = [
     {
       path: "/admin",
-      element: <PrivateRoute element={<LayoutIndex />} />,
+      element: <PrivateRoute element={<AdminIndex />} />,
       children: [
        
       ],
